572f9d144362fd7fe89adb2430734d4bd736469e
simonychev <geneksim@mail.ru>
2023-09-26 07:10:14 +0300

    edit domain name

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  api: 'https://sim-test/api/',
-  serverStaticPath: 'https://sim-test/images/'
+  api: 'https://sim-test.ru/api/',
+  serverStaticPath: 'https://sim-test.ru/images/'
 };
